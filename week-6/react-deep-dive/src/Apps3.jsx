59ef9724b0d5cb8576c1c126d5b09e56533104af
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-01-22 03:07:19 +0530

    week-6

--- week-6/react-deep-dive/src/Apps3.jsx
+++ week-6/react-deep-dive/src/Apps3.jsx
@@ -5,6 +5,13 @@ function Apps() {
   const [count, setCount] = useState(0);
   const [inputValue, setInputValue] = useState(0);
 
+  // if we use useEffect with another state variable, then it will render two times for two state changes. useMemo is used for performing expensive operations and useEffect is for lifecycle events. useMemo for some certain scenarios lets us avoid using extra state variables. ==> memoizing values across re-renders, only recalculate when the dependency changes.(here inputValue)
+
+  // useCallback => returns functions => memoize functions -- memoize function signature
+  // useMemo => returns number/string => memoize values -- synchronous ops, async ops we use useEffect -- it can also return functions which is same as useCallback -> return () => {}
+
+  // setCount doesn't set count value synchronously, it sets the value asynchronously. so we don't pass the value instead we pass a function c => c+1 , so that it utilises the previous current value. in case of consecutive changes we want to make.
+
   let counter = useMemo(() => {
     console.log("memo called");
     let summation = 0;
--- week-6/react-deep-dive/src/Apps4.jsx
+++ week-6/react-deep-dive/src/Apps4.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 import { memo, useEffect, useCallback } from "react";
 
+// useCallback used to memoize functions which can help in optimizing the performance of your application, especially in cases involving child components that rely on reference equality to prevent unnecessary renders.
+
+// React.memo is used to optimize re-renders. If only the props passed to the component change then the re-render happens for the memoized component.
+
+// reference equality ==> var a = {} !== var a = {} (object/functions) ==> function a() & var a are not same. so react might be confused. even though the function is not changing but they are referencially changing according to react.
+
+// parent (re-renders) ==> child (re-renders) even though state variables that are inputs to the child didn't change ==> unless it is being wrapped in a memo.
+
+// instead of defining as functions we can define them as variables. (let, const), in that way we can use useCallback to prevent reference unequality => re-rendering.
+
+// two functions defined separately with same body are referencially not equal.
+
 var a = 1;
 function Apps() {
   const [counter, setCounter] = useState(0);
-  var a = useCallback(() => {}, []);
+  var a = useCallback(() => {
+    console.log("Hi World!");
+  }, []);
   const logSomething = useCallback(() => {
     console.log("Hello");
   }, []);
@@ -16,7 +30,7 @@ function Apps() {
   );
 }
 
-const Child = memo((logSomething) => {
+const Child = memo(({ inputFunction }) => {
   console.log("child render");
 
   return (
