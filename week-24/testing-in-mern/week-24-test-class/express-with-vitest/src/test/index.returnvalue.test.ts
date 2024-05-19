2ec3e58aeb43b94f4688c2cf14ac299cb520bf25
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-05-19 21:37:02 +0530

    spying

--- week-24/testing-in-mern/week-24-test-class/express-with-vitest/src/test/index.returnvalue.test.ts
+++ week-24/testing-in-mern/week-24-test-class/express-with-vitest/src/test/index.returnvalue.test.ts
@@ -14,9 +14,19 @@ describe("POST /sum", () => {
       result: 3,
     });
 
+    vi.spyOn(prismaClient.sum, "create");
+
     const res = await request(app).post("/sum").send({
       a: 1,
       b: 2,
+    });
+
+    expect(prismaClient.sum.create).toHaveBeenCalledWith({
+      data: {
+        a: 1,
+        b: 2,
+        result: 3,
+      },
     });
 
     expect(res.statusCode).toBe(200);
