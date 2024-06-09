37bb3fc82f745ad24855d45b2f0eb39a2bf8c4c4
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-06-10 01:41:14 +0530

    week-25 additions

--- week-25/week-25-live-cypress/scripts/run-e2e.sh
+++ week-25/week-25-live-cypress/scripts/run-e2e.sh
@@ -1,3 +1,5 @@
 # starts db
 # starts server
 # also we need to check this in our local environment, not the production url, so need to change the cy.visit("") to localhost as well.
+# start frontend
+# start backend
